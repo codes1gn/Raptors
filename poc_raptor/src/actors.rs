6262df960f842bc49fc1d0aa0e5077d37a808c9b
Albert Shi <codefisheng@gmail.com>
2022-06-17 20:01:26 +0800

    impl actor receive and on_compute that process msg: DummyWorkload

--- poc_raptor/src/actors.rs
+++ poc_raptor/src/actors.rs
@@ -3,6 +3,29 @@
 use crate::messages;
 
 // placehold for actors
+pub struct Actor {
+    id: usize,
+}
+
+impl Actor {
+    pub fn new(id: usize) -> Actor {
+        return Self {
+            id: id,
+        }
+    }
+
+    pub fn id(&self) -> usize {
+        self.id
+    }
+
+    pub fn receive(&self, msg: messages::DummyWorkload) -> () {
+        self.on_compute(msg);
+    }
+
+    fn on_compute(&self, workload: messages::DummyWorkload) -> () {
+        workload.mock_run();
+    }
+}
 
 // unit tests
 #[cfg(test)]
@@ -23,6 +46,21 @@ mod tests {
         let load = messages::DummyWorkload::new(16);
         let now = time::Instant::now();
         load.mock_run();
+        assert!(now.elapsed() >= time::Duration::from_millis(16));
+    }
+
+    #[test]
+    fn query_actor_id() {
+        let actor = Actor::new(17);
+        assert_eq!(actor.id(), 17);
+    }
+
+    #[test]
+    fn receive_workload() {
+        let actor = Actor::new(1);
+        let load = messages::DummyWorkload::new(16);
+        let now = time::Instant::now();
+        actor.receive(load);
         assert!(now.elapsed() >= time::Duration::from_millis(16));
     }
 }
