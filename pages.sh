4f1e9e399d335c63cf1292853df9f9d3e7753b03
tianyu.wang <tianyu.wang@enflame-tech.com>
2022-07-04 14:32:12 +0800

    name fixed to raptors

--- pages.sh
+++ pages.sh
@@ -1,4 +1,4 @@
 cargo doc --no-deps
 rm -rf ./docs
-echo "<meta http-equiv=\"refresh\" content=\"0; url=poc_raptor\">" > target/doc/index.html
-cp -r target/doc/poc_raptor ./docs
+echo "<meta http-equiv=\"refresh\" content=\"0; url=raptors\">" > target/doc/raptors/index.html
+cp -r target/doc/raptors ./docs
