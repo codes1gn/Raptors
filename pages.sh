7a93d5b98dc210732ec036b7f47dd9ce48121a15
Albert Shi <codefisheng@gmail.com>
2022-07-04 16:20:34 +0800

    update scripts for make docs; fix launch-path from arbitrary dir;

--- pages.sh
+++
@@ -1,4 +0,0 @@
-cargo doc --no-deps
-rm -rf ./docs
-echo "<meta http-equiv=\"refresh\" content=\"0; url=raptors\">" > target/doc/raptors/index.html
-cp -r target/doc/raptors ./docs
---
+++ scripts/make_doc.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# unify entry path for this script
+current_path=`dirname $0`
+raptors_root=`realpath $current_path"/.."`
+
+cd $raptors_root
+
+# build doc
+cargo doc --no-deps
+
+# update to github pages source directory
+rm -rf ./docs
+echo "<meta http-equiv=\"refresh\" content=\"0; url=raptors\">" > target/doc/raptors/index.html
+cp -r target/doc/raptors ./docs
