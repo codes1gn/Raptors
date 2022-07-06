309e7d7c3b7729a4afd9353fa29997691c12b1d5
tianyu.wang <tianyu.wang@enflame-tech.com>
2022-07-06 10:44:13 +0800

    test CI github action

--- docs/implementors/core/cmp/trait.PartialEq.js
+++
@@ -1,3 +0,0 @@
-(function() {var implementors = {};
-implementors["raptors"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.57.0/core/cmp/trait.PartialEq.html\" title=\"trait core::cmp::PartialEq\">PartialEq</a>&lt;<a class=\"enum\" href=\"raptors/messages/enum.TypedMessage.html\" title=\"enum raptors::messages::TypedMessage\">TypedMessage</a>&gt; for <a class=\"enum\" href=\"raptors/messages/enum.TypedMessage.html\" title=\"enum raptors::messages::TypedMessage\">TypedMessage</a>","synthetic":false,"types":["raptors::messages::TypedMessage"]}];
-if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
--- docs/raptors/messages/sidebar-items.js
+++
@@ -1,1 +0,0 @@
-initSidebarItems({"enum":[["TypedMessage",""]],"struct":[["DummyWorkload",""]]});
