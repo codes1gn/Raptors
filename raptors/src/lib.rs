ae31d22ad9b50eef3fdea0cc7ecee5d091ef702f
Albert Shi <codefisheng@gmail.com>
2022-07-08 11:04:29 +0800

    refactor out system-builder into new mod

--- raptors/src/lib.rs
+++ raptors/src/lib.rs
@@ -2,6 +2,7 @@ pub mod actors;
 pub mod executor;
 pub mod messages;
 pub mod system;
+pub mod system_builder;
 pub mod system_config;
 
 /// Prelude module for users to import
@@ -13,6 +14,7 @@ pub mod prelude {
     pub use crate::actors::Actor;
     pub use crate::executor::Executor;
     pub use crate::messages::{SystemCommand, TypedMessage, Workload};
-    pub use crate::system::{System, SystemBuilder};
+    pub use crate::system::{System};
+    pub use crate::system_builder::{SystemBuilder};
     pub use crate::system_config::SystemConfig;
 }
--- raptors/src/system.rs
+++ raptors/src/system.rs
@@ -2,48 +2,6 @@ use crate::actors::*;
 use crate::messages::*;
 use crate::system_config::SystemConfig;
 
-/// Definition: The helper that provide helper functions for system creation
-/// this class wraps all the complex logic used to build elements in;
-/// actor system, for convenient.
-///
-/// backdoors for mocking tests are also provided by this helper
-///
-/// ```
-/// use raptors::prelude::*;
-///
-/// let sys_builder = SystemBuilder::new();
-/// let sys_config = SystemConfig::new();
-/// let syst = sys_builder.build_with_config("mock system", sys_config);
-/// assert_eq!(syst.name(), "mock system".to_string());
-/// ```
-///
-#[derive(Default)]
-pub struct SystemBuilder {
-    cfg: Option<SystemConfig>,
-}
-
-impl SystemBuilder {
-    pub fn new() -> Self {
-        SystemBuilder::default()
-    }
-
-    pub fn use_config(&mut self, config: SystemConfig) -> () {
-        self.cfg = Some(config);
-    }
-
-    pub fn build_with_config(&self, name: &str, config: SystemConfig) -> System {
-        let num_of_actors = config.num_of_actors().unwrap_or_default();
-        println!("{:?}", num_of_actors);
-        System::new(name)
-    }
-
-    pub fn build(&self, name: &str) -> System {
-        let config = self.cfg.as_ref().expect("failed to unwrap config");
-        println!("{:?}", config);
-        // TODO build system with configs
-        System::new(name)
-    }
-}
 
 /// System is the actor system that manages all the actors, supervisors and message channels
 ///
